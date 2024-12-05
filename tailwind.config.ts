30713bea30f48bd22807998858c3a82b1f5693bf
Osman Seytnazarov <seytnazarov.o@gmail.com>
2024-12-05 06:47:23 +0600

    Theme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,24 +4,24 @@ const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
         brand: {
-          50: "rgb(239, 246, 255)",
-          100: "rgb(219, 234, 254)",
-          200: "rgb(191, 219, 254)",
-          300: "rgb(147, 197, 253)",
-          400: "rgb(96, 165, 250)",
-          500: "rgb(59, 130, 246)",
-          600: "rgb(37, 99, 235)",
-          700: "rgb(29, 78, 216)",
-          800: "rgb(30, 64, 175)",
-          900: "rgb(30, 58, 138)",
+          50: "rgb(250, 250, 250)",
+          100: "rgb(245, 245, 245)",
+          200: "rgb(229, 229, 229)",
+          300: "rgb(212, 212, 212)",
+          400: "rgb(163, 163, 163)",
+          500: "rgb(115, 115, 115)",
+          600: "rgb(38, 38, 38)",
+          700: "rgb(64, 64, 64)",
+          800: "rgb(38, 38, 38)",
+          900: "rgb(23, 23, 23)",
         },
         neutral: {
+          0: "rgb(255, 255, 255)",
           50: "rgb(250, 250, 250)",
           100: "rgb(245, 245, 245)",
           200: "rgb(229, 229, 229)",
@@ -32,6 +32,7 @@ const config: Config = {
           700: "rgb(64, 64, 64)",
           800: "rgb(38, 38, 38)",
           900: "rgb(23, 23, 23)",
+          950: "rgb(10, 10, 10)",
         },
         error: {
           50: "rgb(254, 242, 242)",
@@ -45,36 +46,149 @@ const config: Config = {
           800: "rgb(153, 27, 27)",
           900: "rgb(127, 29, 29)",
         },
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        warning: {
+          50: "rgb(255, 247, 237)",
+          100: "rgb(255, 237, 213)",
+          200: "rgb(254, 215, 170)",
+          300: "rgb(253, 186, 116)",
+          400: "rgb(251, 146, 60)",
+          500: "rgb(249, 115, 22)",
+          600: "rgb(234, 88, 12)",
+          700: "rgb(194, 65, 12)",
+          800: "rgb(154, 52, 18)",
+          900: "rgb(124, 45, 18)",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+        success: {
+          50: "rgb(240, 253, 244)",
+          100: "rgb(220, 252, 231)",
+          200: "rgb(187, 247, 208)",
+          300: "rgb(134, 239, 172)",
+          400: "rgb(74, 222, 128)",
+          500: "rgb(34, 197, 94)",
+          600: "rgb(22, 163, 74)",
+          700: "rgb(21, 128, 61)",
+          800: "rgb(22, 101, 52)",
+          900: "rgb(20, 83, 45)",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
+        "brand-primary": "rgb(38, 38, 38)",
+        "default-font": "rgb(23, 23, 23)",
+        "subtext-color": "rgb(115, 115, 115)",
+        "neutral-border": "rgb(229, 229, 229)",
+        white: "rgb(255, 255, 255)",
+        "default-background": "rgb(255, 255, 255)",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      fontSize: {
+        caption: [
+          "12px",
+          {
+            lineHeight: "16px",
+            fontWeight: "400",
+            letterSpacing: "0em",
+          },
+        ],
+        "caption-bold": [
+          "12px",
+          {
+            lineHeight: "16px",
+            fontWeight: "600",
+            letterSpacing: "0em",
+          },
+        ],
+        body: [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+            letterSpacing: "0em",
+          },
+        ],
+        "body-bold": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "600",
+            letterSpacing: "0em",
+          },
+        ],
+        "heading-3": [
+          "16px",
+          {
+            lineHeight: "20px",
+            fontWeight: "600",
+            letterSpacing: "0em",
+          },
+        ],
+        "heading-2": [
+          "20px",
+          {
+            lineHeight: "24px",
+            fontWeight: "600",
+            letterSpacing: "0em",
+          },
+        ],
+        "heading-1": [
+          "30px",
+          {
+            lineHeight: "36px",
+            fontWeight: "600",
+            letterSpacing: "0em",
+          },
+        ],
+        "monospace-body": [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+            letterSpacing: "0em",
+          },
+        ],
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        caption: "Inter",
+        "caption-bold": "Inter",
+        body: "Inter",
+        "body-bold": "Inter",
+        "heading-3": "Inter",
+        "heading-2": "Inter",
+        "heading-1": "Inter",
+        "monospace-body": "monospace",
+      },
+      boxShadow: {
+        sm: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+        default: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+        md: "0px 4px 16px -2px rgba(0, 0, 0, 0.08), 0px 2px 4px -1px rgba(0, 0, 0, 0.08)",
+        lg: "0px 12px 32px -4px rgba(0, 0, 0, 0.08), 0px 4px 8px -2px rgba(0, 0, 0, 0.08)",
+        overlay:
+          "0px 12px 32px -4px rgba(0, 0, 0, 0.08), 0px 4px 8px -2px rgba(0, 0, 0, 0.08)",
+      },
+      borderRadius: {
+        sm: "4px",
+        md: "8px",
+        DEFAULT: "8px",
+        lg: "12px",
+        full: "9999px",
+      },
+      container: {
+        padding: {
+          DEFAULT: "16px",
+          sm: "calc((100vw + 16px - 640px) / 2)",
+          md: "calc((100vw + 16px - 768px) / 2)",
+          lg: "calc((100vw + 16px - 1024px) / 2)",
+          xl: "calc((100vw + 16px - 1280px) / 2)",
+          "2xl": "calc((100vw + 16px - 1536px) / 2)",
+        },
+      },
+      spacing: {
+        112: "28rem",
+        144: "36rem",
+        192: "48rem",
+        256: "64rem",
+        320: "80rem",
+      },
+      screens: {
+        mobile: {
+          max: "767px",
+        },
       },
     },
   },
